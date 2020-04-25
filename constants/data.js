dcc3f4d9937bdb58fe8f398386d14ec128c9d0bd
srikanth gurram <2gurram@gmail.com>
2020-04-25 10:08:47 +0530

    Added new images

--- constants/data.js
+++ constants/data.js
@@ -27,12 +27,12 @@ export default {
         },
         {
         "id" : "6",
-        "name" : "Bug",
+        "name" : "Butterfly",
         "src" : require('../assets/images/96px/icons8-butterfly-96.png'),
         },
         {
         "id" : "7",
-        "name" : "Butterfly",
+        "name" : "Butterfly Side View",
         "src" : require('../assets/images/96px/icons8-butterfly-side-view-96.png'),
         },
         {
@@ -82,14 +82,239 @@ export default {
         },
         {
         "id" : "17",
-        "name" : "Duck",
+        "name" : "Pig with Lipstick",
         "src" : require('../assets/images/96px/icons8-pig-with-lipstick-96.png'),
         },
         {
         "id" : "17",
-        "name" : "Duck",
+        "name" : "Zebra",
         "src" : require('../assets/images/96px/icons8-zebra-96.png'),
-        }          
+        },
+        {
+            "id" : "18",
+            "name" : "Hamster",
+            "src" : require('../assets/images/96px/icons8-cute-hamster-96.png'),
+        },
+        {
+            "id" : "19",
+            "name" : "Fly",
+            "src" : require('../assets/images/96px/icons8-fly-96.png'),
+        },
+        {
+            "id" : "20",
+            "name" : "Spider",
+            "src" : require('../assets/images/96px/icons8-spider-96.png'),
+        },
+        {
+            "id" : "21",
+            "name" : "Cow",
+            "src" : require('../assets/images/96px/icons8-cow-96.png'),
+        },
+        {
+            "id" : "22",
+            "name" : "Donkey",
+            "src" : require('../assets/images/96px/icons8-donkey-96.png'),
+        },
+        {
+            "id" : "20",
+            "name" : "Whale",
+            "src" : require('../assets/images/96px/icons8-whale-96.png'),
+        },
+        {
+            "id" : "21",
+            "name" : "Bear",
+            "src" : require('../assets/images/96px/icons8-bear-96.png'),
+        },
+        {
+            "id" : "22",
+            "name" : "Camel",
+            "src" : require('../assets/images/96px/icons8-camel-96.png'),
+        },
+        {
+            "id" : "23",
+            "name" : "Squirrel",
+            "src" : require('../assets/images/96px/icons8-squirrel-96.png'),
+        },
+        {
+            "id" : "24",
+            "name" : "Snail",
+            "src" : require('../assets/images/96px/icons8-snail-96.png'),
+        },
+        {
+            "id" : "25",
+            "name" : "Rhinoceros",
+            "src" : require('../assets/images/96px/icons8-rhinoceros-96.png'),
+        },
+        {
+            "id" : "26",
+            "name" : "Dog House",
+            "src" : require('../assets/images/96px/icons8-dog-house-96.png'),
+        },
+        {
+            "id" : "27",
+            "name" : "Peacock",
+            "src" : require('../assets/images/96px/icons8-peacock-96.png'),
+        },
+        {
+            "id" : "28",
+            "name" : "Owl",
+            "src" : require('../assets/images/96px/icons8-owl-96.png'),
+        },
+        {
+            "id" : "29",
+            "name" : "Cat",
+            "src" : require('../assets/images/96px/icons8-cat-96.png'),
+        },
+        {
+            "id" : "30",
+            "name" : "Dog",
+            "src" : require('../assets/images/96px/icons8-dog-96.png'),
+        },
+        {
+            "id" : "31",
+            "name" : "Puppy",
+            "src" : require('../assets/images/96px/icons8-puppy-96.png'),
+        },
+        {
+            "id" : "28",
+            "name" : "Crab",
+            "src" : require('../assets/images/96px/icons8-crab-96.png'),
+        },
+        {
+            "id" : "29",
+            "name" : "Lobster",
+            "src" : require('../assets/images/96px/icons8-lobster-96.png'),
+        },
+        {
+            "id" : "30",
+            "name" : "Octopus",
+            "src" : require('../assets/images/96px/icons8-octopus-96.png'),
+        },
+        {
+            "id" : "31",
+            "name" : "Turtle",
+            "src" : require('../assets/images/96px/icons8-turtle-96.png'),
+        },
+        {
+            "id" : "32",
+            "name" : "Dolphin",
+            "src" : require('../assets/images/96px/icons8-dolphin-96.png'),
+        },
+        {
+            "id" : "33",
+            "name" : "Clown Fish",
+            "src" : require('../assets/images/96px/icons8-clown-fish-96.png'),
+        },
+        {
+            "id" : "34",
+            "name" : "Roach",
+            "src" : require('../assets/images/96px/icons8-roach-96.png'),
+        },
+        {
+            "id" : "35",
+            "name" : "Fox",
+            "src" : require('../assets/images/96px/icons8-fox-96.png'),
+        },
+        {
+            "id" : "36",
+            "name" : "Elephant",
+            "src" : require('../assets/images/96px/icons8-elephant-96.png'),
+        },
+        {
+            "id" : "37",
+            "name" : "Gorilla",
+            "src" : require('../assets/images/96px/icons8-gorilla-96.png'),
+        },
+        {
+            "id" : "38",
+            "name" : "Giraffe",
+            "src" : require('../assets/images/96px/icons8-giraffe-full-body-96.png'),
+        },
+        {
+            "id" : "39",
+            "name" : "Kangaroo",
+            "src" : require('../assets/images/96px/icons8-kangaroo-96.png'),
+        },
+        {
+            "id" : "40",
+            "name" : "Mouse",
+            "src" : require('../assets/images/96px/icons8-mouse-animal-96.png'),
+        },
+        {
+            "id" : "41",
+            "name" : "Snake",
+            "src" : require('../assets/images/96px/icons8-snake-96.png'),
+        },
+        {
+            "id" : "42",
+            "name" : "Frog",
+            "src" : require('../assets/images/96px/icons8-frog-96.png'),
+        },
+        {
+            "id" : "43",
+            "name" : "Dog Walking",
+            "src" : require('../assets/images/96px/icons8-dog-walking-96.png'),
+        },
+        {
+            "id" : "44",
+            "name" : "Parrot",
+            "src" : require('../assets/images/96px/icons8-parrot-96.png'),
+        },
+        {
+            "id" : "45",
+            "name" : "Rabbit",
+            "src" : require('../assets/images/96px/icons8-rabbit-96.png'),
+        },
+        {
+            "id" : "46",
+            "name" : "Lion",
+            "src" : require('../assets/images/96px/icons8-lion-96.png'),
+        },
+        {
+            "id" : "47",
+            "name" : "Leopard",
+            "src" : require('../assets/images/96px/icons8-leopard-96.png'),
+        },
+        {
+            "id" : "48",
+            "name" : "Black Jaguar",
+            "src" : require('../assets/images/96px/icons8-black-jaguar-96.png'),
+        },
+        {
+            "id" : "49",
+            "name" : "Deer",
+            "src" : require('../assets/images/96px/icons8-deer-96.png'),
+        },
+        {
+            "id" : "50",
+            "name" : "Dinosaur",
+            "src" : require('../assets/images/96px/icons8-dinosaur-96.png'),
+        },
+        {
+            "id" : "51",
+            "name" : "Beaver",
+            "src" : require('../assets/images/96px/icons8-beaver-96.png'),
+        },
+        {
+            "id" : "52",
+            "name" : "Seal",
+            "src" : require('../assets/images/96px/icons8-seal-96.png'),
+        },
+        {
+            "id" : "53",
+            "name" : "Seahourse",
+            "src" : require('../assets/images/96px/icons8-seahorse-96.png'),
+        },
+        {
+            "id" : "54",
+            "name" : "Prawn",
+            "src" : require('../assets/images/96px/icons8-prawn-96.png'),
+        },
+        {
+            "id" : "55",
+            "name" : "Grasshopper",
+            "src" : require('../assets/images/96px/icons8-grasshopper-96.png'),
+        }                                                                                                                                                                                                     
     ],
     levels:[
         {"id":0, "name":"0", "rounds": 0},
